896f82215d5fab73bdf34d1890e15177311b5813
Praveen MK <praveen.mk@outlook.com>
2019-03-13 20:11:29 +0530

    Enable eureka

--- src/main/java/org/praveenmk/microservice/learn/moviedetails/MovieDetailsApplication.java
+++ src/main/java/org/praveenmk/microservice/learn/moviedetails/MovieDetailsApplication.java
@@ -5,9 +5,11 @@ import org.praveenmk.microservice.learn.moviedetails.repository.MovieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieDetailsApplication {
 
 	public static void main(String[] args) {
